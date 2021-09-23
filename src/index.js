e6083137bc6e7d668c84193e829c97ccc3e0ae4b
Manuel Alférez <manuelalferezruiz@gmail.com>
2021-09-23 12:44:07 +0200

    Example of info window and click listeners

--- src/index.js
+++ src/index.js
@@ -1,18 +1,38 @@
 let map;
 
 function initMap() {
-  var myLatlng = new google.maps.LatLng(37.94359553891993, -4.213611913745383);
-  var mapOptions = {
-    zoom: 16,
-    center: myLatlng,
-  };
-  var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-
-  var marker = new google.maps.Marker({
-    position: myLatlng,
-    title: "Hello World!",
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 12,
+    center: { lat: 34.84555, lng: -111.8035 },
   });
+  // Set LatLng and title text for the markers. The first marker (Boynton Pass)
+  // receives the initial focus when tab is pressed. Use arrow keys to
+  // move between markers; press tab again to cycle through the map controls.
+  const tourStops = [
+    [{ lat: 34.8791806, lng: -111.8265049 }, "Boynton Pass"],
+    [{ lat: 34.8559195, lng: -111.7988186 }, "Airport Mesa"],
+    [{ lat: 34.832149, lng: -111.7695277 }, "Chapel of the Holy Cross"],
+    [{ lat: 34.823736, lng: -111.8001857 }, "Red Rock Crossing"],
+    [{ lat: 34.800326, lng: -111.7665047 }, "Bell Rock"],
+  ];
+  // Create an info window to share between markers.
+  const infoWindow = new google.maps.InfoWindow();
 
-  // To add the marker to the map, call setMap();
-  marker.setMap(map);
+  // Create the markers.
+  tourStops.forEach(([position, title], i) => {
+    const marker = new google.maps.Marker({
+      position,
+      map,
+      title: `${i + 1}. ${title}`,
+      label: `${i + 1}`,
+      optimized: false,
+    });
+
+    // Add a click listener for each marker, and set up the info window.
+    marker.addListener("click", () => {
+      infoWindow.close();
+      infoWindow.setContent(marker.getTitle());
+      infoWindow.open(marker.getMap(), marker);
+    });
+  });
 }
