21a0beb4a1e6a01c8c42d0680c372df9a29f9ba5
Manuel Alférez <manuelalferezruiz@gmail.com>
2021-09-28 11:35:17 +0200

    Reading from two files and showing them in a map

--- src/index.js
+++ src/index.js
@@ -1,10 +1,40 @@
+import { dataFirst } from "../data/dataFirst.js";
+import { dataSecond } from "../data/dataSecond.js";
+
 let map;
+
+let geometry = [];
+
+function getGeometry(features) {
+  features.map((feature) => {
+    geometry.push({
+      lat: feature.geometry.coordinates[1],
+      lng: feature.geometry.coordinates[0],
+    });
+  });
+}
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     zoom: 14,
-    center: { lat: 37.9443, lng: -4.2141 },
+    center: { lat: 37.94977521934078, lng: -4.208680637460683 },
   });
-  // NOTE: This uses cross-domain XHR, and may not work on older browsers.
-  map.data.loadGeoJson("../data/track.geojson");
+
+  const firstObject = JSON.parse(JSON.stringify(dataFirst));
+  const secondObject = JSON.parse(JSON.stringify(dataSecond));
+
+  getGeometry(firstObject.features);
+  getGeometry(secondObject.features);
+
+  const runningPath = new google.maps.Polyline({
+    path: geometry,
+    geodesic: true,
+    strokeColor: "#FF0000",
+    strokeOpacity: 1.0,
+    strokeWeight: 2,
+  });
+
+  runningPath.setMap(map);
 }
+
+initMap();
